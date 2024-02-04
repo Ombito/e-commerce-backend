d058c974fb881d9db1b05e2e3ee976e1a6a04d30
Ombito <alvinombito09@gmail.com>
2024-02-04 16:23:18 +0300

    Feat: add app.py

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from models import User, db
+from flask_cors import CORS
+from flask import Flask, jsonify, request, session, make_response
+from flask_restful import Api, Resource
+from flask_migrate import Migrate
+
+from datetime import timedelta
+from flask_session import Session
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR']= True
+
+db.init_app(app)
+api = Api(app)
+migrate = Migrate(app, db)
+Session(app)
+
+class Index(Resource):
+    def get(self):
+        response_body = '<h1>Hello World</h1>'
+        status = 200
+        headers = {}
+        return make_response(response_body,status,headers)
+    
+api.add_resource(Index,'/', endpoint='landing')
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
